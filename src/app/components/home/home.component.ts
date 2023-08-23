cb24bf0baf0695f8565bb15d28277cdf828f7ea3
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-08-23 13:56:29 +0800

    home bar will read currently logged-in user from user service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/user';
+import { UserService } from 'src/app/shared/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor() {
-  
+export class HomeComponent implements OnInit {
+
+  user: User;
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit () {
+    this.userService.user$.subscribe((user) => this.user = user);
   }
 }
