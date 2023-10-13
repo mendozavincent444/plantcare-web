cfb9dde729f6590497942b1443494f0c893a10c9
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-10-13 16:51:13 +0800

    navbar limited access by role

--- src/app/components/home/navbar/navbar.component.ts
+++ src/app/components/home/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/models/user';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  currentUser: User;
+  constructor(private userService: UserService) {}
+
+
+  ngOnInit(): void {
+    this.userService.getUser().subscribe(user => this.currentUser = user);
+  }
+
+  isRoleAdmin(): boolean {
+    return this.currentUser.role === "ROLE_ADMIN" ? true : false;
+  }
+
+  isRoleSuperAdmin(): boolean {
+    return this.currentUser.role === "ROLE_SUPERADMIN" ? true : false;
+  }
+  
 }
