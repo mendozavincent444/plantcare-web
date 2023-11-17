25b49074cda27aff94500cea75202ce59eeb3728
Vincent Gabriel Mendoza <mendozavincent444@gmail.com>
2023-11-18 00:04:24 +0800

    fixed background color duplicate after leaving components

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ForgotPasswordRequestDto } from 'src/app/shared/payload/forgot-password-request-dto';
 import { AuthService } from 'src/app/shared/services/auth.service';
@@ -8,18 +8,30 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent implements OnInit, OnDestroy {
 
   forgotPasswordRequestForm: FormGroup;
+  private originalBackgroundColor: string;
+
 
   constructor(private authService: AuthService) {
+    this.originalBackgroundColor = document.body.style.background;
     document.body.style.background = 'rgba(23, 122, 23, 0.6)';
+  }
+  
+
+  private resetBackgroundColor(): void {
+    document.body.style.background = this.originalBackgroundColor;
   }
 
   ngOnInit(): void {
     this.forgotPasswordRequestForm = new FormGroup({
       "email": new FormControl(null, [Validators.required, Validators.email])
     });
+  }
+
+  ngOnDestroy(): void {
+    this.resetBackgroundColor();
   }
 
   onSubmitRequest() {
